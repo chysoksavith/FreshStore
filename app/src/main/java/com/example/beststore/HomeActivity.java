52f8d37b9e04df81c2fcdecf83ea5229f6aca0b1
chysoksavith <120388241+chysoksavith@users.noreply.github.com>
2023-11-15 15:01:50 +0700

    donw with HomePage

---
+++ app/src/main/java/com/example/beststore/HomeActivity.java
@@ -0,0 +1,26 @@
+package com.example.beststore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+
+
+    public void Register(View view) {
+        startActivity(new Intent(HomeActivity.this,RegistrationActivity2.class));
+    }
+
+    public void Login(View view) {
+        startActivity(new Intent(HomeActivity.this,LoginActivity.class));
+    }
+}
