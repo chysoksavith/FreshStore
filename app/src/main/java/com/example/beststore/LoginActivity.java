082f43e3b6a4817509e2ff6ecbe38359eb538161
chysoksavith <120388241+chysoksavith@users.noreply.github.com>
2023-11-15 14:37:19 +0700

    done with login layout and onlick Sign in btn load to register

--- app/src/main/java/com/example/beststore/LoginActivity.java
+++ app/src/main/java/com/example/beststore/LoginActivity.java
@@ -2,13 +2,41 @@ package com.example.beststore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button signIn;
+    EditText email,password;
+    TextView signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signIn = findViewById(R.id.buttonLoginLogin);
+        email = findViewById(R.id.edEmailLogin);
+        password = findViewById(R.id.edPasswordLogin);
+        signUp = findViewById(R.id.tvRegisterNow);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegistrationActivity2.class));
+
+            }
+        });
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/beststore/RegistrationActivity2.java
+++ app/src/main/java/com/example/beststore/RegistrationActivity2.java
@@ -2,13 +2,41 @@ package com.example.beststore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegistrationActivity2 extends AppCompatActivity {
 
+    Button signUp;
+    EditText name,email,password;
+    TextView signIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration2);
+
+        signUp = findViewById(R.id.buttonRegisterRegister);
+        name = findViewById(R.id.edLastNameRegister);
+        email = findViewById(R.id.edEmailRegister);
+        password = findViewById(R.id.edPasswordRegister);
+        signIn = findViewById(R.id.sign_in);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegistrationActivity2.this, LoginActivity.class));
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
