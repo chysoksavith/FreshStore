95cd81a8430c03a4e89a079495cb9cf6995a7d18
chysoksavith <120388241+chysoksavith@users.noreply.github.com>
2023-11-16 15:24:55 +0700

    done with fetch item from cloud

--- app/src/main/java/com/example/beststore/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/beststore/ui/home/HomeFragment.java
@@ -5,22 +5,67 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.beststore.Models.PopularModel;
 import com.example.beststore.R;
+import com.example.beststore.adapter.PopularAdapter;
 import com.example.beststore.databinding.FragmentHomeBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
-
+    RecyclerView popularRec;
+    FirebaseFirestore db;
     //popularitem
 
+    List<PopularModel> popularModelList;
+    PopularAdapter popularAdapter;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        db = FirebaseFirestore.getInstance();
+        popularRec = root.findViewById(R.id.pop_rec);
+
+        //pop items
+
+        popularRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,  false));
+        popularModelList = new ArrayList<>();
+        popularAdapter = new PopularAdapter(getActivity(),popularModelList);
+        popularRec.setAdapter(popularAdapter);
+
+        db.collection("PopularProduct")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            // Clear the list to prevent duplicates
+
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                PopularModel popularModel = document.toObject(PopularModel.class);
+                                popularModelList.add(popularModel);
+                                popularAdapter.notifyDataSetChanged();
+                            }
+                        } else {
+                            Toast.makeText(getActivity(), "Error: " + task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
         return root;
     }
 
