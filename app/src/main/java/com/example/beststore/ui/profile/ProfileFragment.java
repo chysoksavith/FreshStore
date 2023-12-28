1e11e109aaf5a2d4771069bf87d28e18117d0196
chysoksavith <120388241+chysoksavith@users.noreply.github.com>
2023-12-28 21:01:31 +0700

    uploade image profile to firebase part 1

--- app/src/main/java/com/example/beststore/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/beststore/ui/profile/ProfileFragment.java
@@ -1,23 +1,94 @@
 package com.example.beststore.ui.profile;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.beststore.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileFragment extends Fragment {
 
+    CircleImageView profilteImg;
+    EditText name, email, number, address;
+    Button update;
 
+    FirebaseStorage storage;
+    FirebaseAuth auth;
+    FirebaseDatabase database;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        storage = FirebaseStorage.getInstance();
+
+
+        profilteImg = root.findViewById(R.id.profile_img);
+        name = root.findViewById(R.id.profile_name);
+        email = root.findViewById(R.id.profile_email);
+        address = root.findViewById(R.id.profile_number);
+        update = root.findViewById(R.id.update);
+
+
+        profilteImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent,33);
+            }
+        });
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateUserProfile();
+            }
+        });
         return root;
     }
+    private void updateUserProfile() {
+    }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(data.getData() != null){
+            Uri profileUri = data.getData();
+            profilteImg.setImageURI(profileUri);
+
+            final StorageReference reference = storage.getReference().child("profile_pricture")
+                    .child(FirebaseAuth.getInstance().getUid());
+
+            reference.putFile(profileUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                    Toast.makeText(getContext(), "Uploaded", Toast.LENGTH_SHORT ).show();
+                }
+            });
+        }
+    }
 }
